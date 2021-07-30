91c96f6daa0f9f287a63031e39cd01dd116ee5c3
NeoCortexFr <sebastien.latouche.28@gmail.com>
2021-07-30 23:10:23 +0200

    header ok

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.scss';
+
+const Header = () => (
+  <header className="header">
+    <h1 className="header_title">The Web <span className="red">- </span>Shop</h1>
+    <p className="header_content">
+      Bienvenue sur notre boutique en ligne
+    </p>
+  </header>
+);
+export default Header;
