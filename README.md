ed21ea18ee1ab2940150f65bcb7523471cfe5cba
NeoCortexFr <sebastien.latouche.28@gmail.com>
2021-07-30 22:39:01 +0200

    Add React & webpack & firsts dependencies

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # webShop
 Online e-commerce for a friend
+
+## Configuration
+Full ReactJs for front.
+
+Dependencies: 
+- webpack
+  - using React Model from O'Clock online School
+- Sass
+- React-router-dom
+- Axios
+- Babel for ES6
+- yarn
+
---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// import reactLogo from './react-logo.svg'; <img src={reactLogo} alt="react logo" />
+import './app.scss';
+
+const App = () => (
+  <div className="app">
+    <h1 className="app_title">Composant : App</h1>
+  </div>
+);
+
+export default App;
